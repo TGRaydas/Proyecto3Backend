5a3dd30ac3c9ed40a882d3e02c8f769549ca5f5a
ignaciofigueroa <ijfigueroa@gmail.com>
2019-09-23 20:57:49 -0300

    Sending pending requests to current_user

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -8,15 +8,22 @@ class FriendsController < ApplicationController
 		friends = []
 		friends_1 = Friend.where(user_receiver_id:params[:user_id], state:2)
 		friends_2 = Friend.where(user_sender_id:params[:user_id], state:2)
+		pending = Friend.where(user_receiver_id:params[:user_id], state:1)
+		pending_out = []
 		friends_1.each do |f|
 			friends.push(Profile.where(user:f.user_sender_id))
 		end
 		friends_2.each do |f|
 			friends.push(Profile.where(user:f.user_receiver_id))
 		end
-		render json:{friends:friends}
-	end
 
+		pending.each do |f|
+			 pending_out.push(Profile.where(user:f.user_sender_id))
+		end
+
+		render json:{friends:friends, pending:pending_out}
+	end
+	
 	def create_requests
 		Friend.create(user_sender: current_user, user_receiver:params[:user_receiver])
 		render json: {status: "ok"}
