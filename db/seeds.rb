77ec0e40f34d716574ce9489f4d498f0b4646d77
ignaciofigueroa <ijfigueroa@gmail.com>
2019-09-23 21:18:26 -0300

    Initial seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ Profile.create(nickname: 'Ignacio ElAngry Figueroa', user_id: 2)
 Profile.create(nickname: 'Vicente ElHacker Correa', user_id: 3)
 Profile.create(nickname: 'Margarita LaAyudante Estevez', user_id: 4)
 Profile.create(nickname: 'Matias SeCayoSaf Recabarren', user_id: 4)
-Friends.create(user_sender_id: 1, user_receiver_id: 2, state: 2)
-Friends.create(user_sender_id: 1, user_receiver_id: 3, state: 2)
-Friends.create(user_sender_id: 1, user_receiver_id: 4, state: 1)
-Friends.create(user_sender_id: 1, user_receiver_id: 5, state: 0)
+Friend.create(user_sender_id: 1, user_receiver_id: 2, state: 2)
+Friend.create(user_sender_id: 1, user_receiver_id: 3, state: 2)
+Friend.create(user_sender_id: 1, user_receiver_id: 4, state: 1)
+Friend.create(user_sender_id: 1, user_receiver_id: 5, state: 0)
 
-Friends.create(user_sender_id: 2, user_receiver_id: 3, state: 2)
-Friends.create(user_sender_id: 2, user_receiver_id: 4, state: 2)
-Friends.create(user_sender_id: 2, user_receiver_id: 5, state: 0)
+Friend.create(user_sender_id: 2, user_receiver_id: 3, state: 2)
+Friend.create(user_sender_id: 2, user_receiver_id: 4, state: 2)
+Friend.create(user_sender_id: 2, user_receiver_id: 5, state: 0)
 
 
 
