3793d44b7dd9adbd7f2e48c124496a85097b586d
ignaciofigueroa <ijfigueroa@gmail.com>
2019-09-23 22:06:45 -0300

    Added state to created_at 3

--- db/migrate/20190923185702_create_profiles.rb
+++ db/migrate/20190923185702_create_profiles.rb
@@ -3,7 +3,7 @@ class CreateProfiles < ActiveRecord::Migration[5.2]
     create_table :profiles do |t|
       t.references :user, foreign_key: true
       t.string :nickname
-
+      t.integer :state
       t.timestamps
     end
   end
