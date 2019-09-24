bf501722d89254d7ac9e8c062aa7648cb8038328
ignaciofigueroa <ijfigueroa@gmail.com>
2019-09-23 22:00:33 -0300

    Added state to created_at

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,10 +6,10 @@ class FriendsController < ApplicationController
 
 	def get_friends
 		friends = []
+		pending_out = []
 		friends_1 = Friend.where(user_receiver_id:params[:user_id], state:2)
 		friends_2 = Friend.where(user_sender_id:params[:user_id], state:2)
 		pending = Friend.where(user_receiver_id:params[:user_id], state:1)
-		pending_out = []
 		friends_1.each do |f|
 			friends.push(Profile.where(user:f.user_sender_id))
 		end
@@ -23,7 +23,7 @@ class FriendsController < ApplicationController
 
 		render json:{friends:friends, pending:pending_out}
 	end
-	
+
 	def create_requests
 		Friend.create(user_sender: current_user, user_receiver:params[:user_receiver])
 		render json: {status: "ok"}
